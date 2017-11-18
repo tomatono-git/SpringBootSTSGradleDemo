e132b5f38af6dd129f2611306914316f02779819
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2017-11-18 21:55:30 +0900

    プロジェクト追加

---
+++ src/main/java/com/example/demo/SpringBootStsGradleDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStsGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStsGradleDemoApplication.class, args);
+	}
+}
