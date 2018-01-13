cfa1ad27964e07f377eff5d3bbd0f6c48e789ee5
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2018-01-14 00:01:13 +0900

    ビルドスクリプトを修正

--- src/main/java/com/example/demo/data/Neo4jHttpTransactionManager.java
+++ src/main/java/com/example/demo/data/Neo4jHttpTransactionManager.java
@@ -4,4 +4,9 @@ import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 
 public class Neo4jHttpTransactionManager extends Neo4jTransactionManager {
 
+//	public Neo4jHttpTransactionManager(Session session) {
+//		super(session);
+//		// TODO 自動生成されたコンストラクター・スタブ
+//	}
+
 }
