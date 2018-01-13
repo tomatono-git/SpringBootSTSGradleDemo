2aea93077a0976699d310c3bff7ac998bd6e6f40
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2018-01-14 01:06:37 +0900

    サブプロジェクトをビルドする

---
+++ src/main/java/com/example/demo/hoge/Hoge.java
@@ -0,0 +1,16 @@
+package com.example.demo.hoge;
+
+import sub.Library;
+
+public class Hoge {
+	private Library hoge;
+
+	public Library getHoge() {
+		return hoge;
+	}
+
+	public void setHoge(Library hoge) {
+		this.hoge = hoge;
+	}
+
+}
